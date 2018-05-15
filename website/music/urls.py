68ea69f0baa7aba4a930d1f21522bd098355edb2
lilwhut <39291017+lilwhut@users.noreply.github.com>
2018-05-15 07:46:25 +0000

    Add files via upload
    django verkafnið

---
+++ website/music/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    # /music/
+    path('', views.IndexView.as_view(), name='index'),
+
+    # /music/<album_id>/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
+
+    path('register/', views.UserFormView.as_view(), name='register'),
+
+]
